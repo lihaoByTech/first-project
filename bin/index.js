0236d6ed07c9b5f51c409f17894b0634da736583
李浩 <1229218235@qq.com>
2022-07-27 15:58:18 +0800

    添加express前端服务容器

---
+++ bin/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// api接口
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.listen(3018);
